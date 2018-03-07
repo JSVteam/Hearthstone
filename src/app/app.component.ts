b447f02f1cdebfc10df6a0a99c543e768d46973b
Jordi <jordisvila@gmail.com>
2018-03-07 17:48:58 -0500

    Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-import { ThemingPage } from '../pages/theming/theming';
+
 
 @Component({
   templateUrl: 'app.html'
