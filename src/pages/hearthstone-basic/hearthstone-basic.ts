c25480dfe2a8807ff10f6982d416e4e64136b111
David <davidmartinezcasado97@gmail.com>
2018-03-09 19:30:07 +0100

    HS_Basic

--- src/pages/hearthstone-basic/hearthstone-basic.ts
+++ src/pages/hearthstone-basic/hearthstone-basic.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Carta_Classic, CartaBasic, CartaHallofFame} from '../../carta';
+import { DataProvider } from '../../providers/data/data';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
+
 
 /**
  * Generated class for the HearthstoneBasicPage page.
@@ -14,8 +18,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'hearthstone-basic.html',
 })
 export class HearthstoneBasicPage {
+  items = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cards_classic:Carta_Classic[] = [];
+  cardsBasic:CartaBasic[] = [];
+  cardsHoF:CartaHallofFame[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _data: DataProvider, public loadingCtrl: LoadingController) {
+    for (let i = 0; i < 30; i++) {
+      this.items.push( this.items.length );
+    }
+
+    this._data.getAllCards().subscribe(
+      data => {
+        this.cards_classic = data ['Classic']
+        this.cardsBasic = data['Basic']
+        this.cardsHoF = data['Hall of Fame']
+      }  
+    
+    )
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.items.push( this.items.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
   }
 
   ionViewDidLoad() {
